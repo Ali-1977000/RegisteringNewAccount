51259fcbcf42d2cd163ac67b6d0f1145f599145a
Ali-1977000 <ali_hasib2003@yahoo.com>
2022-08-27 21:46:40 +0100

    Formatted CuckesRunner class code.

--- src/test/java/runner/CukesRunner.java
+++ src/test/java/runner/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "stepDefinition",
-        dryRun =false,
+        dryRun = false,
         tags = "@register"
 
 )
