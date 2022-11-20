4d5185723db38274d42e764163f4e634a87bfdcd
Ali Hasib <ahasib@permierfarnell.com>
2022-11-20 15:41:51 +0000

    Re-format the code.

--- src/test/java/stepDefinition/ProducatStepsDef.java
+++ src/test/java/stepDefinition/ProducatStepsDef.java
@@ -17,17 +17,19 @@ public class ProducatStepsDef {
 
 
     Producat producat = new Producat();
+
     @Given("user navigate to login page")
     public void user_navigate_to_login_page() {
         Driver.get().get(ConfigurationReader.get("elemnt14Url"));
     }
+
     @Then("the user on the login page")
     public void the_user_on_the_login_page() {
         logger.info("the user on the login page");
         MyUtlities.sleep(3);
         String expected = "Log In";
         String actual = producat.logIn.getText();
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @And("the user clicks the login button")
@@ -35,12 +37,14 @@ public class ProducatStepsDef {
         producat.logIn.click();
         logger.info("the user clicks the login button");
     }
+
     @Then("user enters the username")
     public void user_enters_the_username() {
         producat.signinName.sendKeys("autotestnonadmin02");
         logger.info("the user clicks the login button");
 
     }
+
     @Then("user enters the password")
     public void user_enters_the_password() {
         producat.password.sendKeys("Password123");
@@ -48,13 +52,15 @@ public class ProducatStepsDef {
 
 
     }
+
     @Then("user click on the sign button")
     public void user_click_on_the_sign_button() {
         MyUtlities.sleep(10);
         producat.signIn.click();
         logger.info("user click on the sign button");
     }
-//    @And("user clicks on the recaptcha")
+
+    //    @And("user clicks on the recaptcha")
 //    public void userClicksOnTheRecaptcha() {
 //        MyUtlities.waitForVisibility(producat.reCaptcha,20);
 //        producat.reCaptcha.click();
@@ -65,12 +71,12 @@ public class ProducatStepsDef {
         Assert.assertTrue(producat.userPofile.isDisplayed());
         logger.info("user is on the home page");
     }
+
     @And("the user accept the cookies")
     public void theUserAcceptTheCookies() {
         producat.cookies.click();
         logger.info("the user accept the cookies");
     }
-
 
 
 }
