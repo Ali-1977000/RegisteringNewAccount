92932b5d23b599b99b5ed770e06584a267536d6d
Ali Hasib <ahasib@permierfarnell.com>
2022-11-20 20:38:50 +0000

    update the code.

--- src/test/java/stepDefinition/LiveStepsDef.java
+++ src/test/java/stepDefinition/LiveStepsDef.java
@@ -13,7 +13,7 @@ public class LiveStepsDef {
     Logger logger = (Logger) LogManager.getLogger(LiveStepsDef.class);
 
 
-    LiveHomePageLoad producat = new LiveHomePageLoad();
+    LiveHomePageLoad liveHomePageLoad = new LiveHomePageLoad();
     @Given("user navigate to login page")
     public void user_navigate_to_login_page() {
         Driver.get().get(ConfigurationReader.get("elemnt14Url"));
@@ -23,24 +23,24 @@ public class LiveStepsDef {
         logger.info("the user on the login page");
         MyUtlities.sleep(3);
         String expected = "Log In";
-        String actual = producat.logIn.getText();
+        String actual = liveHomePageLoad.logIn.getText();
         Assert.assertEquals(expected,actual);
     }
 
     @And("the user clicks the login button")
     public void theUserClicksTheLoginButton() {
-        producat.logIn.click();
+        liveHomePageLoad.logIn.click();
         logger.info("the user clicks the login button");
     }
     @Then("user enters the username")
     public void user_enters_the_username() {
-        producat.signinName.sendKeys("autotestnonadmin02");
+        liveHomePageLoad.signinName.sendKeys("autotestnonadmin02");
         logger.info("the user clicks the login button");
 
     }
     @Then("user enters the password")
     public void user_enters_the_password() {
-        producat.password.sendKeys("Password123");
+        liveHomePageLoad.password.sendKeys("Password123");
         logger.info("user enters the password");
 
 
@@ -48,23 +48,23 @@ public class LiveStepsDef {
     @Then("user click on the sign button")
     public void user_click_on_the_sign_button() {
         MyUtlities.sleep(10);
-        producat.signIn.click();
+        liveHomePageLoad.signIn.click();
         logger.info("user click on the sign button");
     }
 //    @And("user clicks on the recaptcha")
 //    public void userClicksOnTheRecaptcha() {
 //        MyUtlities.waitForVisibility(producat.reCaptcha,20);
-//        producat.reCaptcha.click();
+//        liveHomePageLoad.reCaptcha.click();
 //    }
     @Then("user is on the home page")
     public void user_is_on_the_home_page() {
         MyUtlities.sleep(3);
-        Assert.assertTrue(producat.userPofile.isDisplayed());
+        Assert.assertTrue(liveHomePageLoad.userPofile.isDisplayed());
         logger.info("user is on the home page");
     }
     @And("the user accept the cookies")
     public void theUserAcceptTheCookies() {
-        producat.cookies.click();
+        liveHomePageLoad.cookies.click();
         logger.info("the user accept the cookies");
     }
 
